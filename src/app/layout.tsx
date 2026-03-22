9a89b83bdd7de0ebdcf34e19cdfa29b74b478f58
GustavoAula <gustavo.2024014368@alunocesul.com>
2026-03-22 10:56:37 -0300

    feat: configuração inicial do projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import { Provider } from "@/components/ui/provider";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          {children}
+        </Provider>
+      </body>
+    </html>
+  );
+}
