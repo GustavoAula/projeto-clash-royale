b38bc17b0f5bd7c57b19f797b8f38cd03da3cca1
GustavoAula <gustavo.2024014368@alunocesul.com>
2026-03-22 15:53:52 -0300

    Header pronto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "supercell.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
